e649694ef1e540a52c7bece344ff44bead3d11cc
Jayanta Mondal <jayanta20011@gmail.com>
2025-05-13 20:22:37 +0530

    Fixed CSS and related changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Table from "./Table";
+import "../App.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App(){
-    return(
-        <div>
-            <Header />
-            <Table />
-            <Footer />
-        </div>
-    );
+function App() {
+  return (
+    <div>
+      <Header />
+      <Table />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,7 +7,7 @@ function Table() {
     async function fetchMyAPI() {
       let response = await api.getStocksDeatil();
       if (response) {
-        setStockData(response.data.data);
+        setStockData(response.data);
       }
     }
     fetchMyAPI();
@@ -19,15 +19,15 @@ function Table() {
   }
 
   return (
-    <div className="tab">
+    <div className="tab" style={{ overflowY: "auto" }}>
       <table className="table table-bordered ">
         <thead>
           <tr>
             <th scope="col">Name</th>
-            <th scope="col">Type</th>
-            <th scope="col">%</th>
+            <th scope="col">Amount</th>
+            {/* <th scope="col">%</th> */}
             <th scope="col">Announcement Date</th>
-            <th scope="col">Record Date</th>
+            {/* <th scope="col">Record Date</th> */}
             <th scope="col">Ex-Dividend Date</th>
           </tr>
         </thead>
@@ -35,12 +35,10 @@ function Table() {
           {stockData.map((stock) => {
             return (
               <tr>
-                <th scope="row">{stock[0]}</th>
-                <td>{stock[1]}</td>
-                <td>{stock[2]}</td>
-                <td>{stock[3]}</td>
-                <td>{stock[4]}</td>
-                <td>{stock[5]}</td>
+                <td>{stock.companyName}</td>
+                <td>{stock.dividend}</td>
+                <td>{stock.announcementDate}</td>
+                <td>{stock.exDate}</td>
               </tr>
             );
           })}
